1a6cd7ae3bc213535578d37a9c634b33e8fcbb06
Curt Grogan <curtgrogan@outlook.com>
2020-05-11 01:44:27 -0400

    Angular Specific coding fun.

---
+++ src/app/courses.component.ts
@@ -0,0 +1,17 @@
+//import a @angular/core 'decorator'? for the class
+// Decorators are a design pattern that is used to separate modification or decoration
+// of a class without modifying the original source code.
+// In AngularJS, decorators are functions that allow a service, directive or filter to be
+// modified prior to its usage.
+// Decorators - AngularJS: Developer Guide
+
+// apply the decorator to the class
+// use @Component() like a function, add fields/properties
+// Called the decorator function, accessing elements
+// extends html vocabulary, defines new elements/tags
+import { Component } from "@angular/core";
+@Component({
+  selector: "courses", // defines element <courses>
+  template: "<h2>Courses</h2>",
+})
+export class CoursesComponent {}
