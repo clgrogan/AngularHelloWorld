3c0c4f8fd32b9324d2a5fa8862ae32c7c62e7bf7
Curt Grogan <curtgrogan@outlook.com>
2020-05-15 11:38:55 -0400

    Registered CoursesComponent in App.Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CoursesComponent } from "./courses.component"; //Import code automatically added when using auto-complete on declartions class addition.
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent, // Component Class Name. In VS Code using auto complete added import above.
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
